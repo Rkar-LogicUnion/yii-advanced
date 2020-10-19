914db47f2cb0683ebe2cd2004e7b2500c53dc654
Rkar-LogicUnion <appleidrkar@icloud.com>
2020-10-20 01:14:51 +0630

    Partial View

---
+++ frontend/views/product/index.php
@@ -0,0 +1,6 @@
+<?php
+
+?>
+
+<h1>Index of Product Page</h1>
+<?=$this->render('menu')?>
---
+++ frontend/views/product/menu.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<ul class="list-group">
+    <li class="list-group-item">
+        <a href="#">Menu 1</a>
+    </li>
+    <li class="list-group-item">
+        <a href="#">Menu 2</a>
+    </li>
+    <li class="list-group-item">
+        <a href="#">Menu 3</a>
+    </li>
+</ul>
